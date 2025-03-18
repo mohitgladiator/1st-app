a2b160cce42e27221694c88d09bc7b92b91b006c
mohitgladiator <mohitsharma125699@gmail.com>
2025-03-18 21:47:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = "journal-cache-v1";
-const urlsToCache = ["/", "/main.html", "/main.css", "/script.js"];
+const CACHE_NAME = "journal-pwa-v1";
+const urlsToCache = [
+  "/1st%20app/",                     
+  "/1st%20app/index.html",            
+  "/1st%20app/style.css",
+  "/1st%20app/script.js",
+  "/1st%20app/manifest.json",
+  "/1st%20app/icons/icon-192x192.png",
+  "/1st%20app/icons/icon-512x512.png"
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
     })
   );
 });
