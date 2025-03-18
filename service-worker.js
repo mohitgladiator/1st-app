3ba367f04f41572ce9b735f26f91933545818ccb
mohitgladiator <mohitsharma125699@gmail.com>
2025-03-18 22:55:51 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,37 @@
-const CACHE_NAME = 'journal-cache-v2';  // Version bump to clear old cache
-const urlsToCache = [
-    '/1st-app/',                         
-    '/1st-app/index.html',
-    '/1st-app/main.css',
-    '/1st-app/script.js',
-    '/1st-app/manifest.json',
-    '/1st-app/icons/icon-192x192.png',
-    '/1st-app/icons/icon-512x512.png'
+const cacheName = "journal-app-v1";
+const assets = [
+  "/1st-app/index.html",  
+  "/1st-app/main.css",     
+  "/1st-app/script.js",    
+  "/1st-app/manifest.json",
+  "/1st-app/icons/icon-192x192.png",
+  "/1st-app/icons/icon-512x512.png"
 ];
 
-// Install event
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-// Activate event
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== cacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
-// Fetch event
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
-            .catch(() => caches.match('/1st-app/index.html'))  // Fallback to index.html on errors
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return cacheRes || fetch(event.request);
+    })
+  );
 });
