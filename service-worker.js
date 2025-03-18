ea7dc1445bebecbef85801e5b0ae2155aa77d8ae
mohitgladiator <mohitsharma125699@gmail.com>
2025-03-18 22:48:09 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,44 @@
-const CACHE_NAME = "journal-pwa-v1";
+const CACHE_NAME = 'journal-cache-v2';  // Version bump to clear old cache
 const urlsToCache = [
-  "/1st%20app/",                     
-  "/1st%20app/index.html",            
-  "/1st%20app/style.css",
-  "/1st%20app/script.js",
-  "/1st%20app/manifest.json",
-  "/1st%20app/icons/icon-192x192.png",
-  "/1st%20app/icons/icon-512x512.png"
+    '/1st-app/',                         
+    '/1st-app/index.html',
+    '/1st-app/main.css',
+    '/1st-app/script.js',
+    '/1st-app/manifest.json',
+    '/1st-app/icons/icon-192x192.png',
+    '/1st-app/icons/icon-512x512.png'
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+            .catch(() => caches.match('/1st-app/index.html'))  // Fallback to index.html on errors
+    );
 });
